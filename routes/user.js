e810441ff85e9b2f204cb2903f23df2ebe84a74a
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-03-20 09:50:14 +0100

    add .env

--- routes/user.js
+++ routes/user.js
@@ -3,141 +3,169 @@ const router = express.Router();
 const User = require("../models/User");
 const { SHA256 } = require("crypto-js");
 const uid2 = require("uid2");
-require('dotenv').config()
+require("dotenv").config();
 
-
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
-const jwt = require('jsonwebtoken');
-const auth = require('../middleware/auth');
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+
+// Convert buffer as base64 format
+const convertToBase64 = (file) => {
+  return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
+};
 
 //registration route
 
-router.post("/registration",  async (req, res) => {
-    const user = await User.findOne({email: req.body.email});
+router.post("/signup", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
 
-    const {name, lastName, nickName, email, address, phoneNumber, profilePicture, password, admin} = req.body;
+  const {
+    name,
+    lastName,
+    nickName,
+    email,
+    address,
+    phoneNumber,
+    profilePicture,
+    password,
+    admin,
+  } = req.body;
 
-    if (user) {
-        res.json("Already exists");
-    } else if (name && lastName && email && password) {
+  if (user) {
+    res.json("Already exists");
+  } else if (name && lastName && email && password) {
+    //handle pasword
+    const salt = uid2(120);
+    const hashed = SHA256(password + salt);
 
-        //handle pasword
-        const salt = uid2(120)
-        const hashed = SHA256(password + salt);
-        
+    // admin
+    const admin = 0;
 
-        //handle profilePicture
-        const publicId = email + 'profilePicture'
-        const result = cloudinary.uploader.upload(profilePicture, {public_id: publicId})
-        result.then((data) => {
-            console.log(data);
-            console.log(data.secure_url);
-        }).catch((err) => {
-            console.log(err);
-        });
-        const imageUrl = cloudinary.url(publicId, {
-            // width: 100,
-            // height: 150,
-            // Crop: 'fill'
-        });
+    //handle profilePicture
+    // const publicId = email + "profilePicture";
+    // const result = cloudinary.uploader.upload(profilePicture, {
+    //   public_id: publicId,
+    // });
+    // result
+    //   .then((data) => {
+    //     console.log(data);
+    //     console.log(data.secure_url);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    // const imageUrl = cloudinary.url(publicId, {
+    //   // width: 100,
+    //   // height: 150,
+    //   // Crop: 'fill'
+    // });
 
+    const newUser = new User({
+      name: name,
+      lastName: lastName,
+      nickName: nickName,
+      email: email,
+      address: address,
+      phoneNumber: phoneNumber,
+      salt: salt,
+      password: hashed,
+      admin: admin,
+      // profilePicture: result,
+    });
 
-        const newUser = new User({
-            name: name,
-            lastName: lastName,
-            nickName: nickName,
-            email: email,
-            address: address,
-            phoneNumber: phoneNumber,
-            profilePicture: profilePicture,
-            salt: salt,
-            password: hashed,
-            admin: admin,
-            profilePicture: imageUrl
-        })
-        await newUser.save();
-        res.json('user created');
-    };
+    const result = await cloudinary.uploader.upload(
+      convertToBase64(req.files.profilePicture),
+      {
+        folder: `airbnb/${newUser._id}`,
+      }
+    );
+
+    newUser.profilePicture = result;
+
+    console.log(result);
+    await newUser.save();
+    res.json({
+      name: name,
+      lastName: lastName,
+      nickName: nickName,
+      email: email,
+      address: address,
+      phoneNumber: phoneNumber,
+      profilePicture: result,
+    });
+  }
 });
 
 // login route
 
 router.put("/login", async (req, res) => {
+  if (req.body.email && req.body.password) {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const password = SHA256(req.body.password + user.salt).toString();
+      if (user.password === password) {
+        const token = jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+          expiresIn: "24h",
+        });
 
-    if (req.body.email && req.body.password){
-        const user = await User.findOne({email: req.body.email});
-        if (user) {
-            const password = SHA256(req.body.password + user.salt).toString();
-            if (user.password === password) {
-
-               
-                const token = jwt.sign(
-                    { userId: user._id },
-                    'RANDOM_TOKEN_SECRET',
-                    { expiresIn: '24h' }
-                );
-
-                res.status(200).json({
-                    userId: user._id,
-                    token: token
-                });
-
-            } else {
-                res.json("wrong password");
-            }
-        } else {
-            res.json("Wrong user or password");
-        }
+        res.status(200).json({
+          userId: user._id,
+          token: token,
+        });
+      } else {
+        res.json("wrong password");
+      }
     } else {
-        res.json('email or password empty');
+      res.json("Wrong user or password");
     }
-
+  } else {
+    res.json("email or password empty");
+  }
 });
 
 //delete route
 
 router.delete("/delete-user/:id", async (req, res) => {
-    await User.findByIdAndDelete(req.params.id);
-    res.json("User deleted");
-    });
+  await User.findByIdAndDelete(req.params.id);
+  res.json("User deleted");
+});
 
 //modification route
 
 router.put("/modification/:id", auth, async (req, res) => {
-    const userInfos = await User.findById(req.params.id);
+  const userInfos = await User.findById(req.params.id);
 
-    if (userInfos){
-        if (req.body.nickName) {
-            userInfos.nickName = req.body.nickName;
-        }
-        if (req.body.email) {
-            userInfos.email = req.body.email;
-        }
-        if (req.body.address) {
-            userInfos.address = req.body.address;
-        }
-        if (req.body.phoneNumber) {
-            userInfos.phoneNumber = req.body.phoneNumber;
-        }
-        if (req.body.profilePicture) {
-            userInfos.profilePicture = req.body.profilePicture;
-        }
-        if (req.body.password) {
-            const newPassword = SHA256(req.body.password + userInfos.salt).toString();
-            userInfos.password = newPassword;
-        }
-
-        await userInfos.save();
-        res.json("user modified");
-    } else {
-        res.json("There's no user");
+  if (userInfos) {
+    if (req.body.nickName) {
+      userInfos.nickName = req.body.nickName;
     }
-})
+    if (req.body.email) {
+      userInfos.email = req.body.email;
+    }
+    if (req.body.address) {
+      userInfos.address = req.body.address;
+    }
+    if (req.body.phoneNumber) {
+      userInfos.phoneNumber = req.body.phoneNumber;
+    }
+    if (req.body.profilePicture) {
+      userInfos.profilePicture = req.body.profilePicture;
+    }
+    if (req.body.password) {
+      const newPassword = SHA256(req.body.password + userInfos.salt).toString();
+      userInfos.password = newPassword;
+    }
 
+    await userInfos.save();
+    res.json("user modified");
+  } else {
+    res.json("There's no user");
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const cors = require("cors");
-require('dotenv').config()
-
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1/vinma").then(() => {
-    console.log("Connected to the DB vinma");
+mongoose.connect(process.env.MONGODB_CONNECT).then(() => {
+  console.log("Connected to the DB vinma");
 });
 
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 app.use(userRoutes);
 
-const postRoutes = require('./routes/post');
+const postRoutes = require("./routes/post");
 app.use(postRoutes);
 
 app.listen(8080, () => {
-    console.log("Server has started");
+  console.log("Server has started");
 });
